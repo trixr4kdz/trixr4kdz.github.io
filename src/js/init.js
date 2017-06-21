d47f65f3b819564d961637c02e9b2b1070731a7f
Trixie Roque <troque@lion.lmu.edu>
2017-06-21 05:01:51 -0700

    Added Contact section

--- src/js/init.js
+++ src/js/init.js
@@ -7,7 +7,7 @@
   }); // end of document ready
 
   // ===== Scroll Slowly Down ====
-  $("#download-button").click(function () {
+  $(".scroll-down").click(function () {
     var buttonRef = $(this).attr("href");
     $('html,body').animate({
         scrollTop: $(buttonRef).offset().top},
@@ -16,15 +16,15 @@
 
   // ===== Scroll Back to Top ====
   $(window).scroll(function() {
-      if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-          $('#return-to-top').fadeIn(200);    // Fade in the arrow
-      } else {
-          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-      }
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
   });
   $('#return-to-top').click(function() {      // When arrow is clicked
-      $('body,html').animate({
-          scrollTop : 0                       // Scroll to top of body
-      }, 500);
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
   });
 })(jQuery); // end of jQuery name space
