36183b39f4e097b81e6e32f4b56d42fe5ad2b51e
Trixie Roque <troque@lion.lmu.edu>
2017-06-21 18:58:44 -0700

    Modified README and minor fixes

--- src/js/init.js
+++ src/js/init.js
@@ -6,28 +6,4 @@
 
   }); // end of document ready
 
-  // ===== Scroll Slowly Down ====
-  $(".scroll-down").click(function () {
-    var buttonRef = $(this).attr("href");
-    console.log("HERE");
-    $("html,body").animate({
-        scrollTop: $(buttonRef).offset().top},
-        "slow");
-  });
-
-
-
-  // ===== Scroll Back to Top ====
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $("#return-to-top").fadeIn(200);    // Fade in the arrow
-    } else {
-      $("#return-to-top").fadeOut(200);   // Else fade out the arrow
-    }
-  });
-  $("#return-to-top").click(function() {      // When arrow is clicked
-    $("body,html").animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-  });
 })(jQuery); // end of jQuery name space
