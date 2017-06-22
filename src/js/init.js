92f31130b3ed06725ae384e350305857b078b9e8
Trixie Roque <troque@lion.lmu.edu>
2017-06-21 17:47:32 -0700

    Minor fixes for now. Will update l8r

--- src/js/init.js
+++ src/js/init.js
@@ -1,29 +1,32 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+    $(".parallax").parallax();
 
   }); // end of document ready
 
   // ===== Scroll Slowly Down ====
   $(".scroll-down").click(function () {
     var buttonRef = $(this).attr("href");
-    $('html,body').animate({
+    console.log("HERE");
+    $("html,body").animate({
         scrollTop: $(buttonRef).offset().top},
-        'slow');
+        "slow");
   });
+
+
 
   // ===== Scroll Back to Top ====
   $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      $("#return-to-top").fadeIn(200);    // Fade in the arrow
     } else {
-      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      $("#return-to-top").fadeOut(200);   // Else fade out the arrow
     }
   });
-  $('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
+  $("#return-to-top").click(function() {      // When arrow is clicked
+    $("body,html").animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
   });
